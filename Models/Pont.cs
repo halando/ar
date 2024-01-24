b1f0206779b9210224f8d26f26f155b25f9173f7
andoattila698@gmail.com <kilot58@gmail.com>
2024-01-24 16:24:01 +0100

    tr

---
+++ Models/Pont.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ar.Models
+{
+    internal class Pont
+    {
+        public Pont()
+        {
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override string? ToString()
+        {
+            return "("+X+":"+Y+")";
+        }
+
+        public Pont(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ar.Models;
+
+Console.WriteLine("Hello, World!");
+
+var elemek = new List<Pont> {
+    new Pont(10,20),
+    new Pont(20,30),
+    new Pont(30,40),
+    new Pont(50,60),
+    new Pont(70,80),
+    new Pont(90,100),
+    new Pont(110,120),
+    new Pont(120,130),
+
+
+
+};
+#endregion
+
+
+
+
+
+Console.WriteLine(String.Join(", ", elemek));
+//var vr = from i in elemek select i.X;
+//var vu = elemek.Select(i => i.X);
+var vr = from i in elemek select new{XX= 2*i.X,YY=2*i.Y};
+var vu = elemek.Select(i => new { XX = 2 * i.X, YY = 2 * i.Y });
+
+var nu = elemek.Where(i => i.X > 10).Select(i => i).OrderBy(i=>i.X).ThenBy(i=>i.Y).ToList();
+foreach (var ele in vr)
+    Console.Write(ele+",");
+Console.WriteLine();
+foreach (var ele in vu)
+    Console.Write(ele+",");
+Console.WriteLine("Nagyobb mint 10");
+//foreach (var ele in nu)
+  //  Console.Write(ele+",");
+
+
+
+    
